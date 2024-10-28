5cba689665041a752bb3f99a680bce303c807808
aarnol <114937598+aarnol@users.noreply.github.com>
2024-10-28 15:13:59 -0600

    switching scenes

--- Assets/climbHandler.cs
+++ Assets/climbHandler.cs
@@ -2,19 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class ClimbHandler : MonoBehaviour
 {
-    public GameObject playerOrigin;
-    private Vector3 initialPosition;
-    private int currentScript = 0; // Ensure it's properly managed elsewhere.
-
+    
     [SerializeField] private GameObject controller; // Can be removed if not used
     public InputActionReference reset;
 
     void Start()
     {
-        initialPosition = playerOrigin.transform.position;
+       
         reset.action.performed += ResetScene; // Subscribe once in Start()
     }
 
@@ -25,40 +23,24 @@ public class ClimbHandler : MonoBehaviour
 
     void ResetScene(InputAction.CallbackContext context)
     {
-        // Find and destroy all rope objects
-        GameObject[] ropeObjects = GameObject.FindGameObjectsWithTag("Rope");
-        foreach (GameObject rope in ropeObjects)
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        Debug.Log("" + currentSceneName);
+        switch (currentSceneName)
         {
-            Destroy(rope);
-        }
-
-        // Reset player position
-        playerOrigin.transform.position = initialPosition;
-
-        // Switch between climbing-related scripts based on currentScript
-        switch (currentScript)
-        {
-            case 0:
-                EnableScript<shakeClimb>(false);
-                EnableScript<CylinderBetweenPoints>(true);
+            case "MainScene":
+                SceneManager.LoadScene("MainScene2");
                 break;
-            case 1:
-                EnableScript<CylinderBetweenPoints>(false);
-                EnableScript<Pull>(true);
+            case "MainScene2":
+                SceneManager.LoadScene("MainScene3");
+                break;
+            case "MainScene3":
+                SceneManager.LoadScene("MainScene");
                 break;
             default:
-                EnableScript<Pull>(false);
-                EnableScript<shakeClimb>(true);
+                SceneManager.LoadScene("MainScene");
                 break;
         }
     }
 
-    void EnableScript<T>(bool enable) where T : MonoBehaviour
-    {
-        var script = gameObject.GetComponent<T>();
-        if (script != null)
-        {
-            script.enabled = enable;
-        }
-    }
+    
 }
